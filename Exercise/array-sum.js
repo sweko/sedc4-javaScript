fcff1e5d91721e52c50892bef39ed533079a8672
sweko <swekster@gmail.com>
2015-11-28 16:33:57 +0100

    array sum stub

---
+++ Exercise/array-sum.js
@@ -0,0 +1,7 @@
+
+var count = enterNumber("enter count");
+var numbers = enterArray(count);
+var sum = sumArray(numbers);
+var strArray = asString(numbers);
+
+document.writeln("Sum of the array "+strArray + " is "+ sum);
