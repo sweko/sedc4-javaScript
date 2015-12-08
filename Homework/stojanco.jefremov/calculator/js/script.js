6ca9cdca37fb81d14d7b9b7cf16e2996e710b322
Stojancho Jefremov <stojanco.jefremov@gmail.com>
2015-12-08 16:48:53 +0100

    CalculatorCleaned

--- Homework/stojanco.jefremov/calculator/js/script.js
+++ Homework/stojanco.jefremov/calculator/js/script.js
@@ -35,18 +35,30 @@
 		function parseAndCalculate(expression, OPERATORS) {
 			var funcs = [multiply, divide, add, subtract];                 // the functions associated with the OPERATORS
 			var tokens = expression.split(/\b/);      // split the string into "tokens" (numbers or OPERATORS)
-			for (var round = 0; round < OPERATORS.length; round++) {          // do this for every sign
+			for (var operatorsIndex = 0; operatorsIndex < OPERATORS.length - 1; operatorsIndex += 2) {          // do this for every sign
 				console.log("tokens at this point: " + tokens.join(" "));
-				for (var place = 0; place < tokens.length; place++) {    // do this for every token
-					if (tokens[place] == OPERATORS[round]) {                         // a sign is found
-						var a = parseInt(tokens[place - 1]);    // convert previous token to number
-						var b = parseInt(tokens[place + 1]);        // convert next token to number
-						var result = funcs[round](a, b);           // call the appropriate function
-						console.log("calculating: " + a + OPERATORS[round] + b + "=" + result);
-						tokens[place - 1] = result.toString();      // store the result as a string
-						tokens.splice(place, 2);	// delete obsolete tokens
-						place--;	//and back up one place
+				for (var tokensIndex = 0; tokensIndex < tokens.length; tokensIndex++) {    // do this for every token
+					var leftOperand = parseInt(tokens[tokensIndex - 1]);    // convert previous token to number
+					var rightOperand = parseInt(tokens[tokensIndex + 1]);	//convert next token to number
+					if (tokens[tokensIndex] == OPERATORS[operatorsIndex]) {                         // a sign is found
+						var result = funcs[operatorsIndex](leftOperand, rightOperand);           // call the appropriate function
+						tokens[tokensIndex - 1] = result.toString();      // store the result as a string
+						tokens.splice(tokensIndex, 2);	// delete obsolete tokens
+						tokensIndex--;	//and back up one place
 					}
+					else if (tokens[tokensIndex] == OPERATORS[operatorsIndex + 1]) {                         // a sign is found
+							if (tokens[tokensIndex] == '-' && isNaN(leftOperand) && !isNaN(rightOperand)) {	//unary subtract
+								leftOperand = 0;
+								result = funcs[operatorsIndex + 1](leftOperand, rightOperand);           // call the appropriate function
+								tokens[tokensIndex] = result.toString();      // store the result as a string
+								tokens.splice(tokensIndex + 1, 1);
+							} else {
+								result = funcs[operatorsIndex + 1](leftOperand, rightOperand);           // call the appropriate function
+								tokens[tokensIndex - 1] = result.toString();      // store the result as a string
+								tokens.splice(tokensIndex, 2);	// delete obsolete tokens
+								tokensIndex--;	//and back up one place
+							}
+						}
 				}
 			}
 			return tokens[0];                  // at the end tokens[] has only one item: the result
@@ -95,6 +107,9 @@ function processInput(input, output, OPERATORS) {
 			var lastChar = output[output.length - 1];
 			if (output != '0' && !isContained(lastChar, OPERATORS)) {
 				output += input;
+			}			
+			else if (output == '0' && input == '-') {
+				output = input;
 			}
 			else if (isContained(lastChar, OPERATORS) && output.length > 1) {
 				output = output.replace(/.$/,input);
