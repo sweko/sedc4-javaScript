9e17ca650c0cbd3cfe0be126f0b8d8cac65ddcba
Marjan Velkovski <marjan.velk@gmail.com>
2015-12-10 03:20:08 +0100

    Create calculator.js

---
+++ Homework/marjan.velkovski/calculator/calculator.js
@@ -0,0 +1,232 @@
+//-----------------------B-----U-----G-----S--------------------------------------
+    //1. If there's 0 on the display and you press any operator, and then any other number 
+    //it displays zero if after that you press any operator except for +, - and =.
+    //---------------FEATURES---YET---TO---BE---ADDED---------------------------------
+    // 1. Keys should change colour when hovered over 
+    // 2. Keys should change colour when pressed or entered on keyboard.
+
+    var numArr = [];                           //All the numeric input is stored here, it gets reset when any non-numeric key gets pressed. 
+    var resultArr = [];                        //The results of all previous operations are stored here.
+    var keyArr = [];                           //All pressed keys, except for C
+    var result = 0;
+    var lastOper = "";                         //The operator that was used last
+    var keyboardInput = [];
+
+
+
+    function typeOfPrevKey (keyArr) {                                              //Checks the type of the previous key 
+        var x = keyArr[keyArr.length - 1];
+        if (!(isNaN(parseInt(x)))){
+            return "num";
+        }else if (x == "=") {
+            return "=";
+        } else if (x == "sq" || x == "√" || x =="<" || x == "±"){
+            return "advFun";
+        } else {
+            return "oper";
+        }
+    }
+
+    document.onkeydown = function(event) {                                          //Manages the keyboard input
+        event = event || window.event;
+        var value = "";
+        var e = event.keyCode;
+        if (e==96 || e == 48){
+            value="0";
+        } else if (e==97 || e == 49){
+            value = "1";
+        } else if (e==98 || e==50) {
+            value = "2";
+        } else if (e==99 || e==51) {
+            value = "3";
+        } else if (e==100 || e==52) {
+            value = "4";
+        } else if (e==101 || e==53) {
+            value = "5";
+        } else if (e==102 || e==54) {
+            value = "6";
+        } else if (e==103 || e==55) {
+            value = "7";
+        } else if (e==104) {
+            value = "8";
+        } else if (e==105 || e==57) {
+            value = "9";
+        }else if (e==16) {
+            value="shift";
+        } else if (e ==187 ) {
+            if (keyboardInput[keyboardInput.length-1] == "shift") {                 //Allows for use of the shift key, so it can tell apart between +/= and 8/*
+                value = "+";
+            } else {
+                value = "=";
+            }
+        } else if (e==56) {
+            if (keyboardInSput[keyboardInput.length-1] == "shift") {
+                value = "X";
+            } else {
+                value = "8";
+            }
+        } else if (e==107){
+            value = "+";
+        } else if (e==106) {
+            value = "X";
+        } else if (e==13){
+            value = "=";
+        } else if (e==109 || e==189){
+            value="-";
+        } else if (e==111 || e==191){
+            value="/";
+        } else if (e==36) {
+            value="<";
+        } else if (e==35) {
+            value="±";
+        } else if (e==33) {
+            value="√";
+        } else if (e==34) {
+            value="sq";
+        } else if (e==27){
+            value="C";
+        }
+
+        keyboardInput.push(value);
+        return mainFunction(value);
+    }
+
+
+    function operation(value) {  //The operations take place here. The operands are the int we get from numArr and the final element of resultArr
+
+                if (value == "+") {
+                        if (resultArr.length == 0){
+                            result = parseInt(numArr.join(""));
+                            document.getElementById("display").innerHTML = result;
+                            resultArr.push(result);
+                        } else {
+                            result = resultArr[resultArr.length-1] + parseInt(numArr.join(""));
+                            document.getElementById("display").innerHTML = result;
+                            resultArr.push(result);
+                        } 
+                } else if (value =="-") {
+                        if (result == 0 && resultArr.length == 0) { 
+                            result = parseInt(numArr.join(""));  
+                            document.getElementById("display").innerHTML = result;  
+                            resultArr.push(result);
+                       } else {     
+                            result = resultArr[resultArr.length-1] - (parseInt(numArr.join("")));
+                            document.getElementById("display").innerHTML = result;
+                            resultArr.push(result);
+                        }
+                } else if (value == "X") {
+                        if (result == 0 && resultArr.length == 0) {
+                            result = parseInt(numArr.join(""));
+                            document.getElementById("display").innerHTML = result;
+                            resultArr.push(result);
+                       } else {     
+                            result = resultArr[resultArr.length-1] * (parseInt(numArr.join("")));
+                            document.getElementById("display").innerHTML = result;
+                            resultArr.push(result);
+                        }
+                } else if (value == "/") {
+                        if (result == 0 && resultArr.length == 0) {
+                            result = parseInt(numArr.join(""));
+                            document.getElementById("display").innerHTML = result;
+                            resultArr.push(result);
+                       } else {     
+                            result = resultArr[resultArr.length-1] / (parseInt(numArr.join("")));
+                            //result = parseInt(result);
+                            document.getElementById("display").innerHTML = result;
+                            resultArr.push(result);
+                        }
+
+                } else if (value == "sq") {
+                        if (result == 0 && resultArr.length == 0) {
+                            result = parseInt(numArr.join("")) * parseInt(numArr.join(""));
+                            document.getElementById("display").innerHTML = result;
+                            resultArr.push(result);
+                        } else {     
+                            result = resultArr[resultArr.length-1] * resultArr[resultArr.length-1];
+                            document.getElementById("display").innerHTML = result;
+                            resultArr.push(result);
+                        }
+                } else if (value == "√") {
+                        if (result == 0 && resultArr.length == 0) {
+                            result = Math.sqrt(parseInt(numArr.join("")));
+                            document.getElementById("display").innerHTML = result;
+                            resultArr.push(result);
+                        } else {     
+                            result = Math.sqrt(resultArr[resultArr.length-1]);
+                            document.getElementById("display").innerHTML = result;
+                            resultArr.push(result);
+                        }
+                } else if (value == "<") {
+                        if (result == 0 && resultArr.length == 0) {
+                            result = (parseInt(numArr.join("")) - (parseInt(numArr.join("")) % 10)) /10;
+                            document.getElementById("display").innerHTML = result;
+                            resultArr.push(result);
+                        } else {     
+                            result = (resultArr[resultArr.length-1] - (resultArr[resultArr.length-1] % 10)) / 10;
+                            document.getElementById("display").innerHTML = result;
+                            resultArr.push(result);
+                        }
+
+                } else {
+                        if (result == 0 && resultArr.length == 0) {
+                            result = parseInt(numArr.join("")) * -1;
+                            document.getElementById("display").innerHTML = result;
+                            resultArr.push(result);
+                        } else {     
+                            result = resultArr[resultArr.length-1] * -1;
+                            document.getElementById("display").innerHTML = result;
+                            resultArr.push(result);
+                        }
+
+                }
+    }        
+
+
+    function mainFunction (value) {                                                  //Handles user input
+
+        if(!(isNaN(parseInt(value))) && numArr.length < 13) {                        //Numbers get stored in numArray. There's 12 digit limit
+                    numArr.push(value);
+                    document.getElementById("display").innerHTML = numArr.join("");
+                    keyArr.push(value);
+
+        } else if (value == "=") {
+                if (typeOfPrevKey(keyArr) == "oper" ) {                               //Doesn't call operation if previous key is an operator                      
+
+                } else if (typeOfPrevKey(keyArr) == "num" && keyArr.length !==0 ){
+                    if (lastOper == ""){
+                    } else {
+                        operation(lastOper);
+                        keyArr.push(value);
+
+                    }
+                } else {
+                        operation(lastOper);
+                        keyArr.push(value);
+                }
+
+        } else if (value == "C") {                                                    //Resets everything
+                    numArr = []; 
+                    resultArr = [];
+                    keyboardInput = []; 
+                    lastOper = ""; 
+                    keyArr = [];
+                    result = 0;
+                    document.getElementById("display").innerHTML = "";
+
+        } else if (value =="sq" || value=="√" || value =="<" || value == "±") {
+              if (typeOfPrevKey(keyArr) !== "oper"){                                //Calls operation() if the previous key wasn't an operator
+                      operation(value);
+              }
+                       numArr = [];
+                keyArr.push(value);
+
+        } else {
+                if (typeOfPrevKey(keyArr) == "num"){                                //Calls operation() if the previous key press was a number
+                        operation(value); 
+                } 
+
+                numArr = [];
+                lastOper = value;                   
+                keyArr.push(value);
+        }
+}
