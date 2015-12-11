11a37c3224ebd5ed399ce2f553e431c4269c2885
Marjan Velkovski <marjan.velk@gmail.com>
2015-12-11 16:21:54 +0100

    Update calculator.js

--- Homework/marjan.velkovski/calculator/calculator.js
+++ Homework/marjan.velkovski/calculator/calculator.js
@@ -1,4 +1,3 @@
-    
     //-----------------------KEYBOARD---INPUT------------------------------------------- 
     //Numbers and +-/*=C where expected.
     //< at HOME, ± at END, √ at PGUP, sq at PGDN
@@ -38,10 +37,7 @@
     }
 
 
-    document.onkeydown = function(event) {                                          //Manages the keyboard input and changes button color on key down
-        event = event || window.event;
-        var value = "";
-        var e = event.keyCode;
+    function keyCodeToValue (e) {
         if (e==96 || e == 48){
             value="0";
         } else if (e==97 || e == 49){
@@ -66,7 +62,7 @@
             value="shift";
         } else if (e ==187 ) {
             if (keyboardInput[keyboardInput.length-1] == "shift") {                 //Allows for use of the shift key, so it can tell apart between +/= and 8/*
-                value = "+";
+                value = "+";                        
             } else {
                 value = "=";
             }
@@ -97,7 +93,16 @@
         } else if (e==27){
             value="C";
         }
-        
+
+        return value;
+
+    }
+
+    document.onkeydown = function(event) {                                          //Manages the keyboard input and changes button color on key down
+        event = event || window.event;
+        var value = "";
+        var e = event.keyCode;
+        value = keyCodeToValue(e);
         keyboardInput.push(value);
         document.getElementById(value).style.background='#7E9AA3';
         return mainFunction(value);
@@ -107,61 +112,21 @@
         event = event || window.event;
         var value = "";
         var e = event.keyCode;
-        if (e==96 || e == 48){
-            value="0";
-        } else if (e==97 || e == 49){
-            value = "1";
-        } else if (e==98 || e==50) {
-            value = "2";
-        } else if (e==99 || e==51) {
-            value = "3";
-        } else if (e==100 || e==52) {
-            value = "4";
-        } else if (e==101 || e==53) {
-            value = "5";
-        } else if (e==102 || e==54) {
-            value = "6";
-        } else if (e==103 || e==55) {
-            value = "7";
-        } else if (e==104) {
-            value = "8";
-        } else if (e==105 || e==57) {
-            value = "9";
-        }else if (e==16) {
-        } else if (e ==187 ) {
-            if (keyboardInput[keyboardInput.length-2] == "shift") {                 
+        if (e ==187 ) {
+            if (keyboardInput[keyboardInput.length-2] == "shift") {                 //Allows for use of the shift key, so it can tell apart between +/= and 8/*
                 value = "+";
             } else {
                 value = "=";
             }
-        } else if (e ==56 ) {
-            if (keyboardInput[keyboardInput.length-2] == "shift") {                 
+        } else if (e==56) {
+            if (keyboardInput[keyboardInput.length-2] == "shift") {
                 value = "X";
             } else {
                 value = "8";
             }
-        } else if (e==107){
-            value = "+";
-        } else if (e==106) {
-            value = "X";
-        } else if (e==13){
-            value = "=";
-        } else if (e==109 || e==189){
-            value="-";
-        } else if (e==111 || e==191){
-            value="/";
-        } else if (e==36) {
-            value="<";
-        } else if (e==35) {
-            value="±";
-        } else if (e==33) {
-            value="√";
-        } else if (e==34) {
-            value="sq";
-        } else if (e==27){
-            value="C";
-        }
-
+        } else {
+            value = keyCodeToValue(e);
+        }         
         document.getElementById(value).style.background='';
     }
 
