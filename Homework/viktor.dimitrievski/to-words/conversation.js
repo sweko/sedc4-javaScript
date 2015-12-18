ec316d243e6aec4f72aab46792f2ea3beb78b830
Viktor <viktor_dimitrievski@hotmail.com>
2015-12-18 17:49:58 +0100

    return is working

--- Homework/viktor.dimitrievski/to-words/conversation.js
+++ Homework/viktor.dimitrievski/to-words/conversation.js
@@ -1,5 +1,6 @@
 var number = 0; //name of varible for storing number from input field
 var result = document.getElementById('displayResult');
+var stingArray = '';
 
 function getValue (){
 	number = document.getElementById('numberFromInput').value; //Geting value from input field and storin in varible number
@@ -9,7 +10,7 @@ function getValue (){
 	//result.innerHTML = "Input number = " + number;
 	var par = parseInt(number); //geting parsed value from input field stored in varible number
 	number = par; // set number value in integer format
-	console.log(number);
+	//console.log(number);
 	if(number <= 0)
 	{
 		result.innerHTML = "Invalid input";
@@ -18,13 +19,8 @@ function getValue (){
 	toWords(number);
 	}
 }
-
-
-
-
 function toWords(number) {
 
-	
 	if(number <=19 ){
 		
 		return toNinety(number);
@@ -37,8 +33,10 @@ function toWords(number) {
 	}else if(number  < 1000){
 		var firstDigit = parseInt(number / 100);
 		toNinety(firstDigit);
-		result.innerHTML += " ";
-		result.innerHTML += "hundred ";
+		//result.innerHTML += " ";
+		//result.innerHTML += "hundred ";
+		stingArray += " ";
+		stingArray += "hundred ";
 		number = parseInt(number % 100);
 		toTensLoop(number);
 	}else if(number < 999999){
@@ -50,8 +48,10 @@ function toWords(number) {
 		}else{
 			toHundredLoop(firstDigit);
 		}
-		result.innerHTML += " ";
-		result.innerHTML += "thousand ";
+		//result.innerHTML += " ";
+		//result.innerHTML += "thousand ";
+		stingArray += " ";
+		stingArray += "thousand ";
 		number = parseInt(number % 1000);
 		toWords(number);
 	}else if(number < 999999999){
@@ -62,9 +62,12 @@ function toWords(number) {
 			toTensLoop(firstDigit);
 		}else{
 			toHundredLoop(firstDigit);
+			//con0
 		}
-		result.innerHTML += " ";
-		result.innerHTML += "milion ";
+		//result.innerHTML += " ";
+		//result.innerHTML += "milion ";
+		stingArray += " ";
+		stingArray += "milion ";
 		number = parseInt(number % 1000000);
 		toWords(number);
 	}else {
@@ -76,12 +79,13 @@ function toWords(number) {
 		}else{
 			toHundredLoop(firstDigit);
 		}
-		result.innerHTML += " ";
-		result.innerHTML += "bilion ";
+		//result.innerHTML += " ";
+		//result.innerHTML += "bilion ";
+		stingArray += " ";
+		stingArray += "bilion ";
 		number = parseInt(number % 1000000000);
 		toWords(number);
 	}
-	
 	
 	
 	function toTensLoop (num){
@@ -95,8 +99,10 @@ function toWords(number) {
 	function toHundredLoop (num){
 		var firstDigit = parseInt(num / 100)
 		toNinety(firstDigit);
-		result.innerHTML += " ";
-		result.innerHTML += "hundred ";
+		//result.innerHTML += " ";
+		//result.innerHTML += "hundred ";
+		stingArray += " ";
+		stingArray += "hundred ";
 		firstDigit = parseInt(num % 100);
 		if (firstDigit < 20){
 			toNinety(firstDigit);
@@ -128,8 +134,10 @@ function toWords(number) {
 		]
 		for(var i= 0; i < 20; i++){
 			if(num === i){
-				result.innerHTML += num1to20InArray[i];
-				result.innerHTML += " ";
+				//result.innerHTML += num1to20InArray[i];
+				//result.innerHTML += " ";
+				stingArray +=(num1to20InArray[i]);
+				stingArray +=(" ");
 			}
 		}
 	}
@@ -145,10 +153,23 @@ function toWords(number) {
 				"eighty", 
 				"ninety"
 		]
-		result.innerHTML += numTens[num-1];
-		result.innerHTML += " ";
+		//result.innerHTML += numTens[num-1];
+		//result.innerHTML += " ";
+		stingArray +=(numTens[num-1]);
+		stingArray +=(" ");
 	}
+	
+	
+	
+	String.prototype.capitalize = function() {
+		
+		return this.charAt(0).toUpperCase() + this.slice(1);
+	}
+	stingArray = stingArray.capitalize();
+	
+	result.innerHTML = stingArray;
+	return stingArray;
+	
 }
-
 
 
