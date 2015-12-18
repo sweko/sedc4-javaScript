f35fec78b821a284828248c8ebd7647f64ab6cb2
ednodete <viktor_dimitrievski@hotmail.com>
2015-12-19 00:53:26 +0100

    Update

--- Homework/viktor.dimitrievski/to-words/conversation.js
+++ Homework/viktor.dimitrievski/to-words/conversation.js
@@ -1,5 +1,6 @@
 var number = 0; //name of varible for storing number from input field
 var result = document.getElementById('displayResult');
+var stringArray = '';
 
 function getValue (){
 	number = document.getElementById('numberFromInput').value; //Geting value from input field and storin in varible number
@@ -15,28 +16,34 @@ function getValue (){
 		result.innerHTML = "Invalid input";
 	}
 	else {
+	stringArray = '';
 	toWords(number);
 	}
 }
 function toWords(number) {
 
 	if(number <=19 ){
-		
-		return toNinety(number);
-		
+		toNinety(number);
 	}else if(number < 100){
+		
 		var tenDigits = parseInt(number / 10);
 		toTens(tenDigits);
 		number = parseInt(number % 10);
 		toNinety(number);
+		
 	}else if(number  < 1000){
-		var firstDigit = parseInt(number / 100);
-		toNinety(firstDigit);
-		result.innerHTML += " ";
-		result.innerHTML += "hundred ";
-		number = parseInt(number % 100);
-		toTensLoop(number);
+		toHundredLoop(number);
+		//var firstDigit = parseInt(number / 100);
+		//toNinety(firstDigit);
+		//result.innerHTML += " ";
+		//result.innerHTML += "hundred ";    //this code can be optimised if you call only function toHundredLoop() because here you are doing double job
+		//stringArray += " ";
+		//stringArray += "hundred ";
+		//number = parseInt(number % 100);
+		//toTensLoop(number);
+		
 	}else if(number < 999999){
+		
 		var firstDigit = parseInt(number / 1000);
 		if(firstDigit < 20){
 		toNinety(firstDigit);
@@ -45,26 +52,41 @@ function toWords(number) {
 		}else{
 			toHundredLoop(firstDigit);
 		}
-		result.innerHTML += " ";
-		result.innerHTML += "thousand ";
+		
+		//result.innerHTML += " ";
+		//result.innerHTML += "thousand ";
+		stringArray += " ";
+		stringArray += "thousand ";
+		
 		number = parseInt(number % 1000);
+		
 		toWords(number);
+		
 	}else if(number < 999999999){
+		
 		var firstDigit = parseInt(number / 1000000);
+		
 		if(firstDigit < 20){
 			toNinety(firstDigit);
 		}else if (firstDigit < 100){
 			toTensLoop(firstDigit);
 		}else{
 			toHundredLoop(firstDigit);
-			//con0
 		}
-		result.innerHTML += " ";
-		result.innerHTML += "milion ";
+		
+		//result.innerHTML += " ";
+		//result.innerHTML += "milion ";
+		stringArray += " ";
+		stringArray += "milion ";
+		
 		number = parseInt(number % 1000000);
+		
 		toWords(number);
+		
 	}else {
+		
 		var firstDigit = parseInt(number / 1000000000);
+		
 		if(firstDigit < 20){
 			toNinety(firstDigit);
 		}else if (firstDigit < 100){
@@ -72,14 +94,19 @@ function toWords(number) {
 		}else{
 			toHundredLoop(firstDigit);
 		}
-		result.innerHTML += " ";
-		result.innerHTML += "bilion ";
+		
+		//result.innerHTML += " ";
+		//result.innerHTML += "bilion ";
+		stringArray += " ";
+		stringArray += "bilion ";
 		number = parseInt(number % 1000000000);
+		
 		toWords(number);
 	}
 	
 	
-	function toTensLoop (num){
+	function toTensLoop (num){ //ja secka desetkata
+	
 		var firstDigit = parseInt(num / 10);
 		toTens(firstDigit);
 		num = parseInt(num % 10);
@@ -87,20 +114,26 @@ function toWords(number) {
 		
 	}
 	
-	function toHundredLoop (num){
+	function toHundredLoop (num){ //tuka ja cepka stotkata
+	
 		var firstDigit = parseInt(num / 100)
 		toNinety(firstDigit);
-		result.innerHTML += " ";
-		result.innerHTML += "hundred ";
+		//result.innerHTML += " ";
+		//result.innerHTML += "hundred ";
+		stringArray += " ";
+		stringArray += "hundred ";
 		firstDigit = parseInt(num % 100);
+		
 		if (firstDigit < 20){
 			toNinety(firstDigit);
 		}else{
 		toTensLoop(firstDigit);
 		}
+		
 	}
-	function toNinety (num) {
-		var num1to20InArray = ["zero",
+	
+	function toNinety (num) { // array with number name from 1 to 19
+		var num1to20InArray = ["",
 						"one",
 						"two",
 						"three",
@@ -121,15 +154,18 @@ function toWords(number) {
 						"eighteen",
 						"nineteen",
 		]
-		for(var i= 0; i < 20; i++){
-			if(num === i){
-				result.innerHTML += num1to20InArray[i];
-				result.innerHTML += " ";
-			}
-		}
+		stringArray += num1to20InArray[num];
+		//for(var i= 0; i < 20; i++){
+		//	if(num === i){
+				//result.innerHTML += num1to20InArray[i];
+				//result.innerHTML += " ";
+		//		stringArray +=(num1to20InArray[i]);
+		//		stringArray +=(" ");
+		//	}
+		//}
 	}
 	
-	function toTens(num){
+	function toTens(num){//array with number name like 20-30-40....
 		var numTens = ["",
 				"twenty",
 				"thirty",
@@ -140,10 +176,24 @@ function toWords(number) {
 				"eighty", 
 				"ninety"
 		]
-		result.innerHTML += numTens[num-1];
-		result.innerHTML += " ";
+		//result.innerHTML += numTens[num-1];
+		//result.innerHTML += " ";
+		stringArray += numTens[num-1];
+		stringArray += " ";
 	}
+	
+	
+	
+	String.prototype.capitalize = function() { // prototip function which make first letter Uppercase
+		
+		return this.charAt(0).toUpperCase() + this.slice(1);
+	}
+	stringArray = stringArray.capitalize();
+	result.innerHTML = stringArray;//printing on screen
+	
+	
+	
+	return stringArray; // finial result is here like string
 }
-
 
 
