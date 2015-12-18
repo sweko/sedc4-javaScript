2ec20f9db7ed565fe0dfa9575d8412dd5fd1d086
sweko <swekster@gmail.com>
2015-12-18 18:43:47 +0100

    dynamic objects

---
+++ Exercise/Objects/academies.js
@@ -0,0 +1,8 @@
+var sedc = new Firm("sedc", "tutunski");
+sedc.totalStudents = 120;
+
+var semos = new Firm("semos", "skopjanka");
+semos.totalStudents = 7;
+
+allFirms.academies = [sedc, semos];
+
---
+++ Exercise/Objects/hospitals.js
@@ -0,0 +1,12 @@
+var sistina = new Firm("sistina", "zajcev rid");
+sistina.totalDoctors = 50;
+
+var neuromedika = new Firm("neuromedika", "partizanska");
+neuromedika["totalDoctors"] = 25;
+
+var remedika = new Firm("remedika", "zelezara");
+remedika.totalDoctors = 40;
+
+
+allFirms.hospitals = [sistina, neuromedika, remedika];
+
