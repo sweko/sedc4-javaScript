a9be94fa70e1768f1bbb2e73a0191a9eace0a95e
sweko <swekster@gmail.com>
2015-12-19 16:41:37 +0100

    Checkbox methods

---
+++ Exercise/Eventica/checkbox.js
@@ -0,0 +1,12 @@
+var printAlert = function(){
+	alert("Hello from alert");
+};
+
+var printConsole = function(){
+	console.log("Hello from console");
+};
+
+var printDiv = function(){
+	var result = document.getElementById("result");
+	result.innerHTML = "Hello from div";
+};
