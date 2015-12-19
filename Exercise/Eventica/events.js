a6f8e3c0b51f38073c5c22a096ba7f23a5684137
sweko <swekster@gmail.com>
2015-12-19 16:39:18 +0100

    Events demos

--- Exercise/Eventica/events.js
+++ Exercise/Eventica/events.js
@@ -23,5 +23,30 @@ document.addEventListener('DOMContentLoaded',  function(){
 
 
 function createCalculatorInput(){
-//
+   var content = document.getElementById("content");
+   
+   
+   
+   var select = document.createElement("select");
+   var operations = [
+	   { text:"Add", value:"+"},
+	   { text:"Subtract", value:"-"},
+	   { text:"Multiply", value:"*"},
+	   { text:"Divide", value:"/"},
+   ];
+   for (var index = 0; index < operations.length; index++) {
+	   var option = document.createElement("option");
+	   option.text = operations[index].text;
+	   option.value = operations[index].value;
+	   select.appendChild(option);
+   }
+   content.appendChild(select);
+   
+   var button = document.createElement("button");
+   button.innerHTML = "Click me!";
+   content.appendChild(button);
+   
+   var resultDiv = document.createElement("div");
+   resultDiv.id="result";
+   content.appendChild(resultDiv);
 }
