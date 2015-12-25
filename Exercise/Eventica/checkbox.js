757febd3d558129be1d6072e3bc90918b5f513ea
sweko <swekster@gmail.com>
2015-12-25 18:18:47 +0100

    Added jedi class

--- Exercise/Eventica/checkbox.js
+++ Exercise/Eventica/checkbox.js
@@ -10,3 +10,12 @@ var printDiv = function(){
 	var result = document.getElementById("result");
 	result.innerHTML = "Hello from div";
 };
+
+var doResult = document.getElementById("doResult");
+doResult.addEventListener("change", function(){
+	if (checked){
+		button.addEventListener("click", printDiv)
+	} else {
+		button.removeEventListener("click", printDiv)
+	}
+})
---
+++ Exercise/StarWars/jedi.js
@@ -0,0 +1,35 @@
+var simulator = simulator || {};
+simulator.Jedi = function(name, power, armor, forceLevel, color){
+	this.name = name;
+	if (power > 10) 
+		this.power=10;
+	else if (power < 0) 
+		this.power = 0;
+	else 
+		this.power = power;
+		
+	this.armor = Math.max(1, Math.min(5, armor));
+	
+	this.forceLevel = Math.max(1, Math.min(5, forceLevel));
+	
+	var isValidColor = false;
+	for (var index = 0; index < simulator.jediColors.length; index++) {
+		var validColor = simulator.jediColors[index];
+		if (color === validColor)
+			isValidColor = true;
+	}
+
+	if (!isValidColor){
+		throw new Error(color +" is not a valid lightsabre color for a jedi");
+	}
+	
+	this.color = color;
+	
+	var health = 100;
+	
+	this.getHealth = function(){
+		return health;
+	};
+};
+
+simulator.jediColors = ['blue', 'green', 'purple', 'yellow']; 
