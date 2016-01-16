1966598ee2d7e347b1a45d42a48e9fe79341fcdd
Wekoslav Stefanovski <swekster@gmail.com>
2016-01-16 15:11:45 +0100

    Day 14 part 1

--- Exercise/AdventOfCodeLive/day14.js
+++ Exercise/AdventOfCodeLive/day14.js
@@ -1,9 +1,9 @@
 processor.process = function (data, container, callback) {
     var regex = /^(.*) can fly (\d*) km\/s for (\d*) seconds, but then must rest for (\d*) seconds.$/gm;
     var match = regex.exec(data);
-    var reindeer = [];
+    var reindeers = [];
     while (match) {
-        reindeer.push({
+        reindeers.push({
             name: match[1],
             speed: Number(match[2]),
             burstTime: Number(match[3]),
@@ -12,7 +12,33 @@ processor.process = function (data, container, callback) {
         match = regex.exec(data);
     }
     
-    container.innerHTML = JSON.stringify(reindeer);
+    var time = 2503;
     
-    return "done";
+    for (var index = 0; index < reindeers.length; index++) {
+        var reindeer = reindeers[index];
+        
+        reindeer.cycleTime = reindeer.burstTime + reindeer.restTime;
+        reindeer.cycleDistance = reindeer.burstTime * reindeer.speed;
+        
+        var completeCycles = Math.floor(time / reindeer.cycleTime);
+        var driveTime =  completeCycles * reindeer.cycleTime;
+
+        var restTime = time - driveTime;
+       
+        var lastCycleDistance;
+        if (restTime < reindeer.burstTime){
+           lastCycleDistance = restTime * reindeer.speed;
+        } else {
+            lastCycleDistance = reindeer.burstTime * reindeer.speed;
+        }
+        
+        var totalDistance = reindeer.cycleDistance * completeCycles + lastCycleDistance;
+        reindeer.totalDistance = totalDistance;
+    }
+    container.innerHTML = JSON.stringify(reindeers);
+    
+    var distances = reindeers.map(function(rd){return rd.totalDistance;});
+    var maxDistance = Math.max.apply(null, distances);
+    
+    callback(maxDistance);
 };
