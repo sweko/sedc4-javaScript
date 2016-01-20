cc3c88d65a06987a058acc728e915231ccac318c
Wekoslav Stefanovski <swekster@gmail.com>
2016-01-20 21:52:24 +0100

    Exercise 1

--- Exercise/Arrays/linq.js
+++ Exercise/Arrays/linq.js
@@ -42,12 +42,32 @@ var objectArray = function (array) {
 
 var booleanArray = function (array) {
     var result = [];
-    for (var index = 0; index < array.length; index++) {
+    var len = array.length;
+    for (var index = 0; index < len; index++) {
         var element = array[index];
         result.push(!!element);
     }
     return result;
 };
 
+
+var genericIterator = function(array, operation){
+    var result = [];
+    var len = array.length;
+    for (var index = 0; index < len; index++) {
+        var element = array[index];
+        result.push(operation(element));
+    }
+    return result;
+};
+
+var squareNumber = function(x){return x*x;};
+var halfNumber = function(x){return x/2;};
+var sinNumber = function(x){return Math.sin(x);};
+var boolNumber = function(x){return !!x;};
+
+var squares = genericIterator(array, squareNumber);
+
+var bools = genericIterator(array, function(x){return !!x;});
 
 
